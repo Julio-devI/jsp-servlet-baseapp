5d3ac246cd428ce176fc60e69c0958c7efc48fa4
luisgcpsantos <gustavosantossbu@gmail.com>
2024-11-12 14:10:19 -0300

    update: atualizando e renomeando a entidade Album e o AlbumDAO

--- src/main/java/br/mendonca/testemaven/dao/AlbumDAO.java
+++ src/main/java/br/mendonca/testemaven/dao/AlbumDAO.java
@@ -1,25 +1,28 @@
 package br.mendonca.testemaven.dao;
-import br.mendonca.testemaven.model.entities.Albuns;
+
+import br.mendonca.testemaven.model.entities.Album;
 import br.mendonca.testemaven.model.entities.User;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AlbunsDAO {
-    public void registerAlbuns(Albuns albuns) throws ClassNotFoundException, SQLException {
+public class AlbumDAO {
+
+    public void registerAlbum(Album album) throws ClassNotFoundException, SQLException {
         Connection conn = ConnectionPostgres.getConexao();
         conn.setAutoCommit(true);
 
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO artists (artistname, listeners, active) values (?,?,?)");
-        ps.setString(1, albuns.getNome());
-        ps.setInt(2, albuns.getReproducoes());
-        ps.setBoolean(3, albuns.isMaisReproduzido());
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO albuns (albumname, tracks, released) values (?,?,?)");
+        ps.setString(1, album.getAlbumname());
+        ps.setInt(2, album.getTracks());
+        ps.setBoolean(3, album.getReleased());
         ps.execute();
         ps.close();
     }
 
-    public List<Albuns> listAllAlbuns() throws ClassNotFoundException, SQLException {
-        ArrayList<Albuns> lista = new ArrayList<Albuns>();
+    public List<Album> listAllAlbum() throws ClassNotFoundException, SQLException {
+        ArrayList<Album> lista = new ArrayList<Album>();
 
         Connection conn = ConnectionPostgres.getConexao();
         conn.setAutoCommit(true);
@@ -28,46 +31,48 @@ public class AlbunsDAO {
         ResultSet rs = st.executeQuery("SELECT * FROM albuns");
 
         while (rs.next()) {
-            Albuns albuns = new Albuns();
-            albuns.setUuid(rs.getString("uuid"));
-            albuns.setNome(rs.getString("Nome Album"));
-            albuns.setReproducoes(rs.getInt("Reproducoes"));
-            albuns.setMaisReproduzido(rs.getBoolean("Mais Reproduzido"));
+            Album album = new Album();
+            album.setUuid(rs.getString("uuid"));
+            album.setAlbumname(rs.getString("albumname"));
+            album.setTracks(rs.getInt("tracks"));
+            album.setReleased(rs.getBoolean("released"));
 
-            lista.add(albuns);
+            lista.add(album);
         }
 
         rs.close();
 
         return lista;
     }
-    public Albuns search(String Nome, Integer reproducoes) throws ClassNotFoundException, SQLException {
-        Albuns albuns = null;
+
+    public Album search(String albumname, Integer tracks) throws ClassNotFoundException, SQLException {
+        Album album = null;
 
         Connection conn = ConnectionPostgres.getConexao();
         conn.setAutoCommit(true);
 
         // Apesar de qualquer SQL funcionar com Statement, a abordagem de usar Prepared Statement evita SQL Injection.
         PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE email = ? AND password = ?");
-        ps.setString(1, Nome);
-        ps.setString(2, reproducoes.toString());
+        ps.setString(1, albumname);
+        ps.setString(2, tracks.toString());
         System.out.println(ps); // Exibe no console do Docker a query j� montada.
 
         ResultSet rs = ps.executeQuery();
         if (rs.next()) {
 
-            albuns = new Albuns();
-            albuns.setUuid(rs.getString("uuid"));
-            albuns.setNome(rs.getString("Nome Album"));
-            albuns.setReproducoes(rs.getInt("Reproducoes"));
-            albuns.setMaisReproduzido(rs.getBoolean("Mais Reproduzido"));
+            album = new Album();
+            album.setUuid(rs.getString("uuid"));
+            album.setAlbumname(rs.getString("albumname"));
+            album.setTracks(rs.getInt("tracks"));
+            album.setReleased(rs.getBoolean("released"));
         }
 
         rs.close();
 
-        return albuns;
+        return album;
     }
-    // TODO: N�o testado
+
+    // TODO: Nao testado
     public List<User> search(String name) throws ClassNotFoundException, SQLException {
         ArrayList<User> lista = new ArrayList<User>();
 
